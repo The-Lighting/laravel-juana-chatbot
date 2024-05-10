0ebbb92e4cec501a7a0521fa2a02c8152f8ac078
OKJ <ojash.ca@Gmail.com>
2024-05-10 15:50:09 +0530

    Added Request a Callback iframe button

--- resources/js/components/components/baseOptions/BaseOptions.jsx
+++ resources/js/components/components/baseOptions/BaseOptions.jsx
@@ -9,7 +9,9 @@ const BaseOptions = (props) => {
     { text: "Know our Instructors", handler: props.actionProvider.handleInstructor, id: 3 },
     { text: "Verify Certificate?", handler: props.actionProvider.handleCertificate, id: 4 },
     { text: "Contact us or Know More", handler: props.actionProvider.handlecontactus, id:5 },
-    { text: "Where did all this start ?",handler: props.actionProvider.handleJuanaNexo, id:6 }
+	{ text: "Request a Callback", handler: props.actionProvider.handlerequestcb, id:6 },
+    { text: "Where did all this start ?",handler: props.actionProvider.handleJuanaNexo, id:7 },
+	{ text: "LMS", handler: props.actionProvider.handleLMS, id: 8 } // Handle Link to LMS
   ];
 
   const optionsMarkup = options.map((option) => (
